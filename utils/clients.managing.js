f60a12f9a1d5b6df7f59c8d0959cdec294cad7ee
Gabriel AMANOU <gabriel.amanou@efrei.net>
2022-12-03 16:47:43 +0100

    clients table managing functions created

---
+++ utils/clients.managing.js
@@ -0,0 +1,78 @@
+pool = require("../utils/db.js");
+
+module.exports = {
+
+    async GetAllclients(){
+        try{
+            let conn = await pool.GetConnection();
+            let sql = 'SELECT * FROM client'
+            const [rows, fields] = await conn.execute(sql);
+            conn.release();
+            console.log("CARS FETCHED: "+rows.length);
+            return rows;
+        }
+        catch (err) {
+            // TODO: log/send error ... 
+            console.log(err);
+            throw err; // return false ???
+        }
+    },
+
+    async GetOneClient(client_id){
+        try{
+            let conn = await pool.GetConnection();
+            let sql = 'SELECT * FROM client where client_id = ?'
+            const [rows, fields] = await conn.execute(sql, [ client_id ]);
+            conn.release();
+            console.log("CARS FETCHED: "+rows.length);
+            if (rows.length == 1) {
+                return rows[0];
+            } else {
+                return false;
+            }
+        }
+        catch (err) {
+            // TODO: log/send error ... 
+            console.log(err);
+            throw err; // return false ???
+        }
+    },
+
+    async DelClient(client_id){
+        try {
+            let conn = await pool.getConnection();
+            let sql = "DELETE FROM client WHERE client_id = ?";
+            const [okPacket, fields] = await conn.execute(sql, [ client_id ]);  // affectedRows, insertId
+            conn.release();
+            console.log("DELETE "+JSON.stringify(okPacket));
+            return okPacket.affectedRows;
+        }
+        catch (err) {
+            console.log(err);
+            throw err; 
+        }
+    },
+
+    async CreateClient(client_name, client_email, client_password){
+        try{
+            let conn = await pool.getConnection();
+            let sql = 'SELECT MAX(client_id) FROM latable'
+            const [maxid, fiels] = await conn.execute(sql);
+            let client_id = maxid[0] + 1;
+            conn.release();
+            console.log("Get Max ID : "+ maxid[0]);
+            conn = await pool.getConnection();
+            sql = 'INSERT into client VALUES (?, ?, ?, ?,)';
+            const [okPacket, fields] = await conn.execute(sql, [client_id, client_name, client_email, client_password]);
+            conn.release();
+            console.log("INSERT "+JSON.stringify(okPacket));
+            return okPacket.insertId;
+        }
+        catch (err) {
+            console.log(err);
+            throw err; 
+        }
+    }
+
+}
+
